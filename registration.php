1b8ea2ca4d92bf95218a363db46249e877330457
mark <mark@rees-solutions.rees.solutions>
2021-12-28 16:14:19 +0000

    di.xml change to allow mariadb versions

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+//Standard Registration Module
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'ReesSolutions_DBOverride',
+    __DIR__
+);
